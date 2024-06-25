3107e6e75c4a84ebd281d5c2d7cf10dc9089a386
SuperGuy123456 <111657887+SuperGuy123456@users.noreply.github.com>
2024-06-24 18:31:45 -0700

    some added changes
    bug in the set clip function

--- player.py
+++ player.py
@@ -1,12 +1,12 @@
 import pygame
+import settings
 pygame.init()
 
 # The player class that is an image and a rectangle that inhibits platformer-style collision
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.Surface((50, 50))
-        self.image.fill((255, 0, 0))
+        self.image = settings.player
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
--- settings.py
+++ settings.py
@@ -1,5 +1,18 @@
+import pygame
+pygame.init()
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FPS = 60
 TITLE = "Platformer"
 AUTHOR = "Manomay Tyagi"
+
+#images
+
+#player 17x23
+
+player = pygame.image.load('assets/characters.png') 
+player = player.set_colorkey((255,255,255))
+player = pygame.Surface.set_clip(player,(0,0,17,23))
+player = pygame.transform.scale(player,(17*2,23*2))
+
+#enemy 18x23
